4053a91f935ecfde140c26a6a63b23bd96efd743
Christopher Nanik <nanik.post@gmail.com>
2025-01-18 04:41:59 +0100

    Initial commit

---
+++ src/main/java/net/chriskatze/katzencraft/KatzencraftMod.java
@@ -0,0 +1,15 @@
+package net.chriskatze.katzencraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KatzencraftMod implements ModInitializer {
+	public static final String MOD_ID = "katzencraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/chriskatze/katzencraft/KatzencraftModClient.java
@@ -0,0 +1,10 @@
+package net.chriskatze.katzencraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KatzencraftModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
